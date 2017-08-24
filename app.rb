5d3316968e10da8982d252dc31ffd7d1ad1d6849
Fabien Loup <info@norydev.com>
2017-08-24 10:38:36 +0300

    more standard error handling

--- app.rb
+++ app.rb
@@ -11,6 +11,7 @@ set :public_folder, File.dirname(__FILE__) + '/public'
 configure :development do
   db = Mongo::Client.new(["127.0.0.1:27017"], database: "hitchspots")
   set :mongo_db, db[:spots]
+  set :show_exceptions, false
 end
 
 configure :production do
@@ -23,20 +24,23 @@ get "/" do
 end
 
 get "/trip" do
-  begin
-    trip = Hitchspots::Trip.new(from: params[:from], to: params[:to])
-    maps_me_kml = trip.spots(format: :kml)
+  trip = Hitchspots::Trip.new(from: params[:from], to: params[:to])
+  maps_me_kml = trip.spots(format: :kml)
 
-    content_type 'text/plain'
-    attachment file_name(params[:from], params[:to])
-    maps_me_kml
-  rescue Hitchspots::NotFound => e
-    @error = { not_found: e.message }
-    erb(:home)
-  rescue Hitchspots::Error => e
-    @error = { external: true }
-    erb(:home)
-  end
+  content_type 'text/plain'
+  attachment file_name(params[:from], params[:to])
+  maps_me_kml
+end
+
+error Hitchspots::NotFound do
+  @error = { message: env["sinatra.error"].message }
+  erb(:home)
+end
+
+error do
+  @error = { message: "Sorry, our service is unavailable at the moment, "\
+                      "please try again later" }
+  erb(:home)
 end
 
 def file_name(from, to)
