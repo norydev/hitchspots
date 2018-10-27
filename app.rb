a373c6cfc367c1262be2bef31b04b0f15af49056
Fabien Loup <info@norydev.com>
2018-10-27 11:33:31 +0200

    HOTFIX: lat/lon from place subparam, not params

--- app.rb
+++ app.rb
@@ -73,8 +73,8 @@ get "/v2/trip" do
   begin
     trip = Hitchspots::Trip.new(
       places: params[:places].sort_by { |index, _| index.to_i }.map do |_, place|
-        Hitchspots::Place.new(place[:name], lat: params[:lat],
-                                            lon: params[:lon])
+        Hitchspots::Place.new(place[:name], lat: place[:lat],
+                                            lon: place[:lon])
       end
     )
 
