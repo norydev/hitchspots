3e9371c2852dea096b112407ec30744bd2a3c1e4
Fabien Loup <info@norydev.com>
2018-11-01 20:24:52 +0100

    add preview map under trip form Only works for trip and requires form to be valid That means, required fields (places name) filled Reset the preview doesn't work yet, so each preview click adds to the map.

--- public/js/hitchspots4.js
+++ public/js/hitchspots4.js
@@ -98,6 +98,129 @@ function toggleAddButton(index) {
   }
 }
 
+function drawMarkers(spots) {
+  var colors = {
+    "1": "#449d44",
+    "2": "#449d44",
+    "3": "#f9e31d",
+    "4": "#ec971f",
+  }
+  var red = "#c9302c";
+
+  markers = spots.map(function(marker) {
+    var el = document.createElement('div');
+
+    // create a HTML element for each feature
+    svg = '<svg height="12" width="12" viewBox="0 0 48 48">' +
+          '  <circle fill="#ffffff" cx="24" cy="24" r="20"/>' +
+          '  <path fill="' + (colors[marker.rating] || red) + '" d="M23.99 4c-11.05 0-19.99 8.95-19.99 20s8.94 20 19.99 20c11.05 0 20.01-8.95 20.01-20s-8.96-20-20.01-20zm8.47 32l-8.46-5.1-8.46 5.1 2.24-9.62-7.46-6.47 9.84-.84 3.84-9.07 3.84 9.07 9.84.84-7.46 6.47 2.24 9.62z"/>' +
+          '</svg>'
+
+    $(el).html(svg)
+
+    // make a marker for each feature and add to the map
+    return new mapboxgl.Marker(el).setLngLat([marker.lon, marker.lat]).addTo(map);
+  });
+}
+
+function drawLine(route) {
+  map.addLayer({
+    "id": "route",
+    "type": "line",
+    "source": {
+      "type": "geojson",
+      "data": {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "LineString",
+          "coordinates": route
+        }
+      }
+    },
+    "layout": {
+      "line-join": "round",
+      "line-cap": "round"
+    },
+    "paint": {
+      "line-color": "#000000",
+      "line-width": 3
+    }
+  });
+}
+
+function centerMap(coords) {
+  var lons = coords.map(function(lon_lat) { return lon_lat[0] });
+  var lats = coords.map(function(lon_lat) { return lon_lat[1] });
+
+  var minLon = Math.min.apply(Math, lons);
+  var maxLon = Math.max.apply(Math, lons);
+  var minLat = Math.min.apply(Math, lats);
+  var maxLat = Math.max.apply(Math, lats);
+
+  // fit to coords window +- 10%
+  map.fitBounds([[
+    minLon - (maxLon - minLon)/10,
+    minLat - (maxLat - minLat)/10
+  ], [
+    maxLon + (maxLon - minLon)/10,
+    maxLat + (maxLat - minLat)/10
+  ]]);
+}
+
+function mapOverlayWaiting() {
+  $("[data-target~='map-preview-action']").html('<span class="text-white">Preview generation...<span>')
+}
+
+function mapOverlayRemove() {
+  $("[data-target~='map-preview-overlay']").hide();
+}
+
+function mapOverlayReset() {
+  var action = $("[data-target~='map-preview-action']");
+
+  if (action.find("button").length == 0) {
+    $("[data-target~='map-preview-action']").html('<button type="button" class="btn btn-primary" onClick="drawTripOnMap()">Preview</button>')
+    $("[data-target~='map-preview-overlay']").show();
+  }
+}
+
+function drawTripOnMap() {
+  var form = $("[data-target~='trip-form']")
+  var formValid = form.find("input").toArray().every(function(input) {
+    return input.checkValidity();
+  })
+
+  if (formValid) {
+    // TODO: reset map here
+    mapOverlayWaiting();
+    var formValues = form.serializeArray()
+    var queryParams = formValues.map(function(formObj) {
+      return formObj.name + '=' + formObj.value
+    });
+
+    var url = "/v2/trip?" + queryParams.join("&");
+
+    $.getJSON(url, function(trip) {
+      mapOverlayRemove();
+      drawMarkers(trip.spots);
+      drawLine(trip.route);
+      centerMap(trip.route);
+    });
+  }
+}
+
+function setMap() {
+  mapboxgl.accessToken = $("#map").attr('data-map-token');
+
+  map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v9',
+    center: [8,47],
+    zoom: 3
+  });
+}
+
 $(document).ready(function() {
   // Geocoding
   engine = new PhotonAddressEngine(); // global var, one engine per page
@@ -109,11 +232,13 @@ $(document).ready(function() {
 
   // Bootstrap tabs
   $('#kind-tabs a').click(function (e) {
-    e.preventDefault()
-    $(this).tab('show')
+    e.preventDefault();
+    $(this).tab('show');
   })
 
   if (window.location.search.substr(1).split("=")[0] === "iso_code"){
-    $('#kind-tabs a[href="#country-tab"]').tab('show')
+    $('#kind-tabs a[href="#country-tab"]').tab('show');
   }
+
+  setMap();
 });
